38c5ebf944d338de6804ff84943eb2cdd87031b1
Yasser Embaby <114987602+embaby0@users.noreply.github.com>
2023-04-19 12:37:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js')
+  }
 
 const body = document.querySelector("body"),
   toggle = document.querySelector(".toggle");
